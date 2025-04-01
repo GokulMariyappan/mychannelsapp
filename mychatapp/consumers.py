b66dd955406aa5a959137efc75d776a4456fa16b
GokulMariyappan <gokulmariyappan1@gmail.com>
2025-04-01 21:33:11 +0530

    did group message thing

--- mychatapp/consumers.py
+++ mychatapp/consumers.py
@@ -1,5 +1,5 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer 
 from asgiref.sync import async_to_sync
 
 class MyConsumer(WebsocketConsumer):
@@ -7,17 +7,24 @@ class MyConsumer(WebsocketConsumer):
 		self.room_group_name = 'messageroom'
 		async_to_sync(self.channel_layer.group_add)(self.room_group_name,self.channel_name)
 		self.accept()
-		self.send(text_data=json.dumps({'message' : 'socket connected to the server!'}))
+		print(self.channel_name)
+		self.send(json.dumps({'message' : 'socket connected to the server!', 'channel_name' : self.channel_name}))
 	 
 	def receive(self, text_data):
+		print(self.channel_name , "recieved a message")
 		recieved_data = json.loads(text_data)
 		message = recieved_data['message']
 		async_to_sync(self.channel_layer.group_send)(self.room_group_name,{
 			'type' : 'chat_message',
-			'message' : message
+			'message' : message,
+			'sender' : self.channel_name
 		})
 	
 	def chat_message(self, event):
-		print(event)
 		message = event['message']
-		self.send(json.dumps({'type' : 'chat', 'message' : message}))
+		if(event['sender'] == self.channel_name):
+			self.send(json.dumps({'senderr' : 'me','type' : 'chat', 'message' : message, 'channel_name' : self.channel_name}))
+			return
+		print(self.channel_name, "this dude sent a message")
+		
+		self.send(json.dumps({'senderr':'them','type' : 'chat', 'message' : message, 'channel_name' : self.channel_name}))
